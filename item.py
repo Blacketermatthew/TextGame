6d6b9879ae9d00f47fbad91b51682621a39e49a7
Matthew Blacketer <matthew.blacketer@epicor.com>
2021-03-06 17:54:57 -0600

    revert

--- item.py
+++ item.py
@@ -109,6 +109,7 @@ rm.dining_hall.place_item(matches)
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 keys = Item("keys")
@@ -123,6 +124,16 @@ keys.set_description("Three keys held together by a rusty keyring.")
 rm.dining_hall_to_garage_hallway.place_item(keys)
 >>>>>>> parent of 3d331e8 (Added win condition for keys)
 
+
+# Garage
+
+### NEED TO MAKE WORKBENCH
+=======
+keys = Item("keys")
+keys.set_description("Three keys held together by a rusty keyring.")
+rm.dining_hall_to_garage_hallway.place_item(keys)
+
+>>>>>>> parent of 3d331e8 (Added win condition for keys)
 
 # Garage
 
